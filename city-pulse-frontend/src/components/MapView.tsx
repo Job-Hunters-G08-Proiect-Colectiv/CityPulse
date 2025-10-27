c517c6800c7998c238e63bb34d05c3a4f2b7d2d2
rilden <9265760+rilden@users.noreply.github.com>
2025-10-27 22:04:44 +0200

    Implement map bounds and zoom limits

--- city-pulse-frontend/src/components/MapView.tsx
+++ city-pulse-frontend/src/components/MapView.tsx
@@ -9,6 +9,7 @@ import MarkerClusterGroup from "react-leaflet-markercluster";
 import { ThumbsUp } from 'lucide-react';
 import "./MapView.css";
 import type { LatLngExpression } from "leaflet";
+import L from "leaflet";
 import type { Report } from "../types/report";
 import { useRef } from "react";
 
@@ -22,11 +23,13 @@ const MapView = ({ reports, onReportClick }: MapViewProps) => {
     reports.length > 0
       ? [reports[0].location.lat, reports[0].location.lng]
       : [46.77, 23.62];
-
+  const topLeftBound = L.latLng(46.876, 23.323);
+  const bottomRightBound = L.latLng(46.688, 23.781);
+  const maxBounds = L.latLngBounds(topLeftBound, bottomRightBound);
   const markerRefs = useRef<any>({});
 
   return (
-    <MapContainer center={position} zoom={13} id="map" zoomControl={false}>
+    <MapContainer center={position} minZoom={12} zoom={13} id="map" zoomControl={false} maxBounds={maxBounds}>
       <TileLayer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution="OpenStreetMap contributors"
