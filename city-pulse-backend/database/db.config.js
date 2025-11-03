d6f299f1da6df5acc1c5e75bfde2d7b637ff3cea
Mark Kurti <kurtimark41@gmail.com>
2025-11-03 19:08:05 +0200

    Database config file added with .env

---
+++ city-pulse-backend/database/db.config.js
@@ -0,0 +1,22 @@
+const { Pool } = require('pg');
+
+// Database connection configuration
+const pool = new Pool({
+    user: process.env.DB_USER || 'postgres',
+    host: process.env.DB_HOST || 'localhost',
+    database: process.env.DB_NAME || 'CityPulseDb',
+    password: process.env.DB_PASSWORD || 'password',
+    port: process.env.DB_PORT || 5432,
+});
+
+// Test connection
+pool.on('connect', () => {
+    console.log('Connected to the database');
+});
+
+pool.on('error', (err) => {
+    console.error('Unexpected error on idle client', err);
+    process.exit(-1);
+})
+
+module.exports = pool;
